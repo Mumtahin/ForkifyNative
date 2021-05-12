d3c99433ef8fa29db850c31157ae653fcfd98347
Mumtahin <n.mumtahin1@gmail.com>
2021-05-12 17:04:30 +0100

    add vertical constraint to search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
@@ -7,8 +7,7 @@ import ResultsList from '../components/ResultsList';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
-    console.log(results[0]);
-
+ 
     const filterResultsByPrice = (price) => {
         return results.filter(result => {
             return result.price === price;
@@ -16,17 +15,19 @@ const SearchScreen = () => {
     };
 
     return (
-        <View>
+        <View style={{flex: 1}}>
             <SearchBar 
                 term={term} 
                 onTermChange={setTerm}
                 onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>Results: {results.length}</Text>
-            <ResultsList results={filterResultsByPrice("£")} title="Cost Effective" />
-            <ResultsList results={filterResultsByPrice("££")} title="Bit Pricier" />
-            <ResultsList results={filterResultsByPrice("£££")} title="Big Spender" />
+            <ScrollView>
+                <Text>Results: {results.length}</Text>
+                <ResultsList results={filterResultsByPrice("£")} title="Cost Effective" />
+                <ResultsList results={filterResultsByPrice("££")} title="Bit Pricier" />
+                <ResultsList results={filterResultsByPrice("£££")} title="Big Spender" />
+            </ScrollView>
         </View>
     )
 };
