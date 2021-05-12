a22951f449c7ef9521727c545019173c16a31887
Mumtahin <n.mumtahin1@gmail.com>
2021-05-12 16:24:23 +0100

    add additional info to results detail

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -5,12 +5,26 @@ const ResultsDetail = ({ result }) => {
 
     return (
         <View>
+            <Image style={styles.image} source={{uri: result.image_url}} />
             <Text>{result.name}</Text>
+            <Text>
+                {result.rating} Stars, {result.review_count} Reviews
+            </Text>
         </View>
     );
 
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: 250,
+        height: 120,
+        borderRadius: 4
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+});
 
 export default ResultsDetail;
