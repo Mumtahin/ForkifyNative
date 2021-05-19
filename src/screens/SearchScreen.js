5e6106119dea212155240cb55edf71300737687e
Mumtahin <n.mumtahin1@gmail.com>
2021-05-19 16:09:03 +0100

    add results details screen with redirect

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,8 @@ import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
-const SearchScreen = () => {
+const SearchScreen = ({navigation}) => {
+    console.log(navigation);
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
  
@@ -23,9 +24,9 @@ const SearchScreen = () => {
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <ScrollView>
-                <ResultsList results={filterResultsByPrice("£")} title="Cost Effective" />
-                <ResultsList results={filterResultsByPrice("££")} title="Bit Pricier" />
-                <ResultsList results={filterResultsByPrice("£££")} title="Big Spender" />
+                <ResultsList navigation={navigation} results={filterResultsByPrice("£")} title="Cost Effective" />
+                <ResultsList navigation={navigation} results={filterResultsByPrice("££")} title="Bit Pricier" />
+                <ResultsList navigation={navigation} results={filterResultsByPrice("£££")} title="Big Spender" />
             </ScrollView>
         </>
     )
